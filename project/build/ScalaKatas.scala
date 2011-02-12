7e16bcc44151c74240bd9bfef4c0dce201e540ec
emilsebastian <emil.jonsson@gmail.com>
2011-02-12 14:53:57 +0100

    Upgraded Scalatest to version 1.3

--- project/build/ScalaKatas.scala
+++ project/build/ScalaKatas.scala
@@ -2,6 +2,6 @@ import sbt._
 
 class ScalaKatasProject(info: ProjectInfo) extends DefaultProject(info) {
 
-    val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
+    val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
 
 }
